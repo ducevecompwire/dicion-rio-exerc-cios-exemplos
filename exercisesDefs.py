dd8f389b1acca04eca4c4080450766d5deb3629f
eduardo.silva <eduardo.silva@compwire.com.br>
2022-09-26 15:42:27 -0300

    adicionando mais um exercício

--- exercisesDefs.py
+++ exercisesDefs.py
@@ -23,7 +23,7 @@ escreva('Hello Word!')
 escreva('Eduardo Ceve da Silva!')"""
 
 # FUNÇÃO DE CONTADOR
-from time import sleep
+'''from time import sleep
 
 
 print('-=' * 30)
@@ -58,7 +58,79 @@ def contador(inicio, fim, passo):
         
         
         
-contador(inicio, fim, passo)
+contador(inicio, fim, passo)'''
+
+# FUMÇÃO PARA VOTAÇÃO
+
+'''ano = int(input('Em que ano você nasceu? '))
+
+def voto(ano):
+    from datetime import date
+    date = date.today().year
+    date = date - ano
     
-     
+    print('-' * date)
+    if date < 16 :
+        print(f'Com {date} anos: NÃO VOTA.')
+    elif date >= 16 and date < 18 :
+        print(f'Com {date} anos: VOTO OPCIONAL.')
+    elif date >= 18 and date < 65 :
+        print(f'Com {date} anos: VOTO OBRIGATÓRIO.')
+    else:
+        print(f'Com {date} anos: VOTO OPCIONAL.')
         
+voto(ano)'''
+     
+# FUNÇÃO PARA CALCULAR FATORIAL
+
+'''def fatorial(n, show = False):
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end=' ')
+            else:
+                print(' = ', end=' ')
+            
+        f *= c
+    return f
+        
+print(fatorial(5, show=True))'''
+
+# FUNÇÃO PARA FICHA DO JOGADOR
+
+'''def ficha(nome, gols):
+    if nome == '':
+        nome = '<desconhecido>'
+    if gols == '':
+        gols = '0'
+    print(f'O jogador {nome} fez {gols} gols no campeonato.')   
+    
+nome = str(input('Nome do jogador: '))
+gols = int(input('Número de gols: '))
+ficha(nome, gols) '''
+
+#FUNÇÃO PARA VALIDAR NÚMERO:
+
+'''def leiaInt (msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')'''
+
+#FUNÇÃO NOTAS E SITUAÇÃO
+
+
+    
